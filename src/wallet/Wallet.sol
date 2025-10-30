3738ae4ee132662a06a827734cdbe75232a3fac8
Pavel Naydanov <pavel.naydanov@metalamp.io>
2025-10-30 14:55:50 +0700

    feat: add callback functions to wallet

--- src/wallet/Wallet.sol
+++ src/wallet/Wallet.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-import { ExecutionHelper } from "@erc7579/core/ExecutionHelper.sol";
-import { ExecutionLib } from "@erc7579/lib/ExecutionLib.sol";
+import {ExecutionHelper} from "@erc7579/core/ExecutionHelper.sol";
+import {ExecutionLib} from "@erc7579/lib/ExecutionLib.sol";
 import {
     ModeLib,
     ModeCode,
@@ -14,12 +14,19 @@ import {
     EXECTYPE_TRY,
     CALLTYPE_SINGLE
 } from "@erc7579/lib/ModeLib.sol";
+import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
 import {IWallet} from "./interfaces/IWallet.sol";
 
-contract Wallet is IWallet, ExecutionHelper {
+contract Wallet is IWallet, ExecutionHelper, ERC1155Holder, ERC721Holder {
     using ModeLib for ModeCode;
     using ExecutionLib for bytes;
+
+    // TODO: storage (для чего мне может понадобиться?)
+    // TODO: подумать над тем, чтобы сделать execute с подписью пользователя
+    // TODO: IERC1271 ???
+    // TODO: IERC165 ???
 
     modifier onlySelf {
         if (msg.sender != address(this)) {
@@ -57,4 +64,7 @@ contract Wallet is IWallet, ExecutionHelper {
             revert UnsupportedCallType(callType);
         }
     }
+
+    /// @notice Allows this contract to receive the chains native token
+    receive() external payable {}
 }
--- test/wallet/Wallet.t.sol
+++ test/wallet/Wallet.t.sol
@@ -6,9 +6,13 @@ import {ModeLib, ModeCode} from "@erc7579/lib/ModeLib.sol";
 import {ExecutionLib} from "@erc7579/lib/ExecutionLib.sol";
 
 import {Wallet, IWallet} from "src/wallet/Wallet.sol";
+import {ERC721Mock} from "../mocks/ERC721Mock.sol";
+import {ERC1155Mock} from "../mocks/ERC1155Mock.sol";
 
 contract WalletTest is Test {
     Wallet public wallet;
+    ERC721Mock public erc721Token;
+    ERC1155Mock public erc1155Token;
 
     StdCheats.Account user;
 
@@ -16,6 +20,8 @@ contract WalletTest is Test {
         user = makeAccount("user");
 
         wallet = new Wallet();
+        erc721Token = new ERC721Mock();
+        erc1155Token = new ERC1155Mock();
 
         vm.startBroadcast(user.key);
 
@@ -55,4 +61,47 @@ contract WalletTest is Test {
 
     // endregion
 
+    // region - Wallet can get other tokens -
+
+    function test_sendERC721() external {
+        address sender = makeAddr("sender");
+        uint256 tokenId = 1;
+
+        erc721Token.mint(sender, tokenId);
+
+        vm.prank(sender);
+        erc721Token.safeTransferFrom(sender, address(wallet), tokenId);
+
+        assertEq(erc721Token.ownerOf(tokenId), address(wallet));
+        assertNotEq(erc721Token.ownerOf(tokenId), sender);
+    }
+
+    function test_sendERC1155() external {
+        address sender = makeAddr("sender");
+        uint256 tokenId = 1;
+        uint256 value = 1;
+
+        erc1155Token.mint(sender, tokenId, value);
+
+        vm.prank(sender);
+        erc1155Token.safeTransferFrom(sender, address(wallet), tokenId, value, "");
+
+        assertEq(erc1155Token.balanceOf(address(wallet), tokenId), value);
+        assertEq(erc1155Token.balanceOf(sender, tokenId), 0);
+    }
+
+    function test_sendNative(uint256 value) external {
+        address sender = makeAddr("sender");
+        deal(sender, value);
+
+
+        vm.prank(sender);
+        (bool success,) = address(wallet).call{value: value}("");
+
+        assertTrue(success);
+        assertEq(address(wallet).balance, value);
+        assertEq(sender.balance, 0);
+    }
+
+    // endregion
 }
