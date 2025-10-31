9e9296550a34f0dd5a83d566e54d938cbe7e261a
Pavel Naydanov <pavel.naydanov@metalamp.io>
2025-10-31 14:38:21 +0700

    test: wallet transfer erc20

--- test/wallet/Wallet.t.sol
+++ test/wallet/Wallet.t.sol
@@ -2,17 +2,20 @@
 pragma solidity 0.8.30;
 
 import {Test, StdCheats, Vm, console} from "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ModeLib, ModeCode} from "@erc7579/lib/ModeLib.sol";
 import {ExecutionLib} from "@erc7579/lib/ExecutionLib.sol";
 
 import {Wallet, IWallet} from "src/wallet/Wallet.sol";
 import {ERC721Mock} from "../mocks/ERC721Mock.sol";
 import {ERC1155Mock} from "../mocks/ERC1155Mock.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 
 contract WalletTest is Test {
     Wallet public wallet;
     ERC721Mock public erc721Token;
     ERC1155Mock public erc1155Token;
+    ERC20Mock public erc20Token;
 
     StdCheats.Account user;
 
@@ -22,6 +25,7 @@ contract WalletTest is Test {
         wallet = new Wallet();
         erc721Token = new ERC721Mock();
         erc1155Token = new ERC1155Mock();
+        erc20Token = new ERC20Mock("Tether USD", "USDT", 6);
 
         vm.startBroadcast(user.key);
 
@@ -35,7 +39,7 @@ contract WalletTest is Test {
 
     function test_execute_transferNative(uint256 amount) external {
         address recipient = makeAddr("recipient");
-        vm.deal(user.addr, amount);
+        deal(user.addr, amount);
 
         ModeCode modeCode = ModeLib.encodeSimpleSingle();
         bytes memory userOpCalldata = ExecutionLib.encodeSingle(recipient, amount, "");
@@ -49,10 +53,49 @@ contract WalletTest is Test {
 
     function test_execute_transferNative_revertIfNotSelf(uint256 amount) external {
         address recipient = makeAddr("recipient");
-        vm.deal(user.addr, amount);
+        deal(user.addr, amount);
 
         ModeCode modeCode = ModeLib.encodeSimpleSingle();
         bytes memory userOpCalldata = ExecutionLib.encodeSingle(recipient, amount, "");
+
+        vm.expectRevert(IWallet.OnlySelf.selector);
+
+        IWallet(user.addr).execute(modeCode, userOpCalldata);
+    }
+
+    // endregion
+
+    // region - User transfer erc-20 token -
+
+    function test_execute_transferERC20(uint256 amount) external {
+        address recipient = makeAddr("recipient");
+        deal(address(erc20Token), user.addr, amount);
+
+        ModeCode modeCode = ModeLib.encodeSimpleSingle();
+        bytes memory userOpCalldata = ExecutionLib.encodeSingle(
+            address(erc20Token),
+            0,
+            abi.encodeWithSelector(IERC20.transfer.selector, recipient, amount)
+        );
+
+        vm.prank(user.addr);
+        IWallet(user.addr).execute(modeCode, userOpCalldata);
+
+        assertEq(erc20Token.balanceOf(recipient), amount);
+        assertEq(erc20Token.balanceOf(user.addr), 0);
+    }
+
+    function test_execute_transferERC20_revertIfNotSelf(uint256 amount) external {
+        address recipient = makeAddr("recipient");
+        deal(address(erc20Token), user.addr, amount);
+
+        ModeCode modeCode = ModeLib.encodeSimpleSingle();
+
+        bytes memory userOpCalldata = ExecutionLib.encodeSingle(
+            address(erc20Token),
+            0,
+            abi.encodeWithSelector(IERC20.transfer.selector, recipient, amount)
+        );
 
         vm.expectRevert(IWallet.OnlySelf.selector);
 
